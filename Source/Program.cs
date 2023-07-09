d077f854ad6e69969465f979b031968ee4349028
David Wiebe <david_wiebe_2@sfu.ca>
2023-07-08 21:03:04 -0700

    Add C#-Raylib base project

---
+++ Source/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.Raymath;
+using static Raylib_cs.KeyboardKey;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Raylib.InitWindow(800, 600, "Hello World!");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.RAYWHITE);
+
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
